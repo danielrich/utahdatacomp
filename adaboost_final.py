cb79d07a55d1b3db69cca35a42f40bd82f5615cf
Daniel Rich <qianyilong@gmail.com>
2014-10-01 05:37:24 -0700

    Starting to do classification.

--- adaboost_final.py
+++ adaboost_final.py
@@ -9,30 +9,42 @@ X = genfromtxt('train_X_final.csv', delimiter=',')
 Y = genfromtxt('train_Y_final.csv', delimiter=',')
 
 X_val = genfromtxt('val_X_final.csv', delimiter=',')
-#Y_val = genfromtxt('val_Y.csv', delimiter=',')
 
-def run_basic_time( model, model_name, X, Y, X_val):
-    new_values = [ [x] for x in range(len(X))]
-    X = numpy.append(X, new_values, 1)
-    from sklearn.preprocessing import StandardScaler
-    scaler = StandardScaler().fit(X)
-    X = scaler.transform(X)
-    max_time_val = X[-1][-1] *2 - X[-2][-1]
 
-    # Load validation data
-    model.fit(X, Y)
+def make_black_maps_class(Y):
+    for i in range(len(Y)):
+        if Y[i] == 600:
+            pass
+        else:
+            Y[i] = 180
+    return Y
 
-    new_values = [ [max_time_val] for x in range(len(X_val))]
-    X_val = numpy.append(X_val, new_values, 1)
+model = ensemble.AdaBoostClassifier()
 
-    # Now predict validation output
-    Y_pred = model.predict(X_val)
 
-    # Crop impossible values
-    Y_pred[Y_pred < 0] = 0
-    Y_pred[Y_pred > 600] = 600
+new_values = [ [x] for x in range(len(X))]
+X = numpy.append(X, new_values, 1)
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler().fit(X)
+X = scaler.transform(X)
+max_time_val = X[-1][-1] *2 - X[-2][-1]
 
-    savetxt('final_pred_y.csv', Y_pred, delimiter=',')
+Y = make_black_maps_class(Y)
+# Load validation data
+import pdb;pdb.set_trace()
+model.fit(X, Y)
+
+new_values = [ [max_time_val] for x in range(len(X_val))]
+X_val = numpy.append(X_val, new_values, 1)
+
+# Now predict validation output
+Y_pred = model.predict(X_val)
+
+# Crop impossible values
+Y_pred[Y_pred < 0] = 0
+Y_pred[Y_pred > 600] = 600
+
+savetxt('final_pred_y.csv', Y_pred, delimiter=',')
 
     # Calculate score
     #err_Y=Y_pred-Y_val
@@ -40,11 +52,6 @@ def run_basic_time( model, model_name, X, Y, X_val):
 
     #print score, model_name, "Basic time"
 
-def run_model( model, model_name, X, Y, X_val):
-
-    run_basic_time(model, model_name, X, Y, X_val)
-
-run_model(ensemble.AdaBoostRegressor(DecisionTreeRegressor(max_depth=3), loss="square", n_estimators=30000), "StandardScaler applied square 3000 Ada Boost regressor modified sklearn with .07x neg", X, Y, X_val)
 #from sklearn import grid_search
 
 #parameters = {"n_estimators":(50,100,200,300,400,500), "base_estimator__max_depth":(2,3,4,5,6,7,8,9,10)}
