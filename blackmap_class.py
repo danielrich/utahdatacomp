6d203304e78791f43c1563e6438657f96f2c1b3d
Daniel Rich <qianyilong@gmail.com>
2014-10-15 21:04:04 -0700

    Generated some graphs and cleaning up the code for presentation.

--- blackmap_class.py
+++ blackmap_class.py
@@ -25,64 +25,24 @@ def make_black_maps_class(Y):
             Y[i] = 180
     return Y
 
-def make_400_class(Y):
-    for i in range(len(Y)):
-        if Y[i] >= 400:
-            Y[i] = 500
-            continue
-        else:
-            Y[i] = 180
-    return Y
-
 def run_model( model, model_name, X, Y, X_val):
 
+    # Add time column to training set.
     new_values = [ [x] for x in range(len(X))]
     X = numpy.append(X, new_values, 1)
-#    from sklearn.preprocessing import StandardScaler # I have a suspicion that the classifier might work better without the scaler
-#    scaler = StandardScaler().fit(X)
-#    X = scaler.transform(X)
     max_time_val = X[-1][-1] *2 - X[-2][-1]
 
     Y_black = make_black_maps_class(Y)
     # Load validation data
     model.fit(X, Y_black)
 
+    # add time column to validation set.
     new_values = [ [max_time_val] for x in range(len(X_val))]
     X_val = numpy.append(X_val, new_values, 1)
 
     # Now predict validation output
     Y_pred = model.predict(X_val)
-    Y = genfromtxt('train_Y_final.csv', delimiter=',')
-
-    Y_four = make_400_class(Y)
-    model.fit(X, Y_four)
-
-    Y_pred_four = model.predict(X_val)
-
-    for i in range(len(Y_pred)):
-        if Y_pred[i] == 180:
-            Y_pred[i] = Y_pred_four[i]
-
-    # Crop impossible values
-    Y_pred[Y_pred < 0] = 0
-    Y_pred[Y_pred > 600] = 600
 
     savetxt('final_pred_y{0}.csv'.format(model_name), Y_pred, delimiter=',')
 
-    black_map_count = 0
-    for y in Y_pred:
-        if y == 600:
-            black_map_count += 1
-
-    print black_map_count, model_name
-    sys.stdout.flush()
-
-
-#run_model(ensemble.AdaBoostClassifier(), "Adaboost class ", X, Y, X_val)
-#run_model(neighbors.KNeighborsClassifier(), "kNN ", X, Y, X_val)
-#run_model(tree.DecisionTreeClassifier(), "decicion tree ", X, Y, X_val)
-#run_model(ensemble.RandomForestClassifier(), "random forest class ", X, Y, X_val)
-run_model(lda.LDA(), "LDA_mixed", X, Y, X_val)
-#run_model(linear_model.LogisticRegression(), "LogisticRegression ", X, Y, X_val)
-#run_model(naive_bayes.GaussianNB(), "gaussianNB ", X, Y, X_val)
-#run_model(svm.SVC(), "svc ", X, Y, X_val)
+run_model(lda.LDA(), "LDA", X, Y, X_val)
--- generate_graphs.py
+++ generate_graphs.py
@@ -6,17 +6,25 @@ import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 
 bad_predictions = genfromtxt('predictions/final_pred_y.csv', delimiter=',')
+ok_predictions = genfromtxt('predictions/final_pred_yLDA_185.csv', delimiter=',')
+best_predictions  = genfromtxt('predictions/final_pred_yprobalities.csv', delimiter=',')
+original_y = genfromtxt('data/train_Y_final.csv', delimiter=',')
 
-num_bins = 50
+num_bins = 300
 # the histogram of the data
-n, bins, patches = plt.hist(bad_predictions, num_bins, normed=1, facecolor='green', alpha=0.5)
+plt.hist(bad_predictions, num_bins, facecolor='green', alpha=0.5)
+plt.hist(original_y, num_bins, facecolor='red', alpha=0.5)
+plt.hist(ok_predictions, num_bins, facecolor='blue', alpha=0.5)
+#plt.hist(best_predictions, num_bins, facecolor='black', alpha=0.5)
 
-plt.plot(bins,'r--')
+#plt.plot(bins,'r--')
 plt.xlabel('Die failure')
 plt.ylabel('frequency of occurences')
-plt.title('Histogram of adaboost predictions')
+plt.title('Histogram of classifying blackmaps')
 
 # Tweak spacing to prevent clipping of ylabel
 #plt.subplots_adjust(left=0.15)
-plt.savefig('graphs/bad_histogram.png')
+plt.xlim([550, 600])
+plt.ylim([0, 600])
+plt.savefig('graphs/zoomed.png')
 
